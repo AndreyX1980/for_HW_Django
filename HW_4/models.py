dbea823bd20631cfcce39cfeca18b20e1fe151fc
AndreyX1980 <aharybin1980@gmail.com>
2023-09-02 21:55:34 +0500

    Д.з. четвертого семинара(1)

--- HW_4/models.py
+++ HW_4/models.py
@@ -1,31 +1,7 @@
 from django.db import models
-
-class Client(models.Model):
-    name = models.CharField(max_length=100)
+class User(models.Model):
+    name = models.CharField(max_length=50)
     email = models.EmailField()
-    phone = models.IntegerField (default=None)
-    address = models.TextField()
-    reg_date = models.DateField()
-
+    age = models.IntegerField()
     def __str__(self):
-       return f'{self.name}, phone: {self.phone}'
-
-class Product(models.Model):
-    title = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    amount = models.IntegerField()
-    add_date = models.DateField()
-
-
-    image = models.ImageField(upload_to='images')
-    def __str__(self):
-       return f'{self.title}, price: {self.price}'
-
-class Order (models.Model):
-    customer = models.ForeignKey(Client, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
-    total_price = models.DecimalField(max_digits=8, decimal_places=2)
-    date_ordered = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.customer} заказал {self.products.all}'
+        return f'Name: {self.name}, email: {self.email}, age:{self.age}'
